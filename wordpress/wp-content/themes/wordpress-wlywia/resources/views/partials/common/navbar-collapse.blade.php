fff7e3112128f9a75fc8769db08ee785e367eab1
Derik Seno <vonnderik@gmail.com>
2025-01-18 18:48:35 +0800

    made navmenu static for viewing

--- wordpress/wp-content/themes/wordpress-wlywia/resources/views/partials/common/navbar-collapse.blade.php
+++ wordpress/wp-content/themes/wordpress-wlywia/resources/views/partials/common/navbar-collapse.blade.php
@@ -5,14 +5,19 @@
   </span>
 </button>
 <div class="collapse navbar-collapse justify-content-end" id="primary-navigation">
-    @if (has_nav_menu('primary_navigation'))
+    {{-- @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu([
             'theme_location' => 'primary_navigation',
             'menu_class'     => 'navbar-nav navbar-nav-icon-desktop',
             'container' => 'ul',
             'walker' => new App\View\Navigations\Main(),
         ]) !!}
-    @endif
+    @endif --}}
+    <ul id="menu-main" class="navbar-nav navbar-nav-icon-desktop">
+      <li id="menu-item-8" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-8 nav-item"><a href="#!" class="nav-link">Product</a></li>
+      <li id="menu-item-9" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-9 nav-item"><a href="#!" class="nav-link">Features</a></li>
+      <li id="menu-item-10" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-10 nav-item"><a href="#!" class="nav-link">Pricing</a></li>
+    </ul>
     <div class="navbar-action d-flex align-items-center">
         <a class="btn btn-link btn-link-header" href="#!">{{ __('Login', 'sage') }}</a>
     </div>
